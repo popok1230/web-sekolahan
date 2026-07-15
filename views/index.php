77355c66322e3b39c62df1d441353e0f74e81fe8
popok1230 <azzam.f.m.1230@gmail.com>
2026-07-15 22:36:10 +0700

    Tambah table awal di index.php

--- views/index.php
+++ views/index.php
@@ -1,1 +1,40 @@
 <?php
+
+include_once "../config/db.php";
+include_once "../app/Siswa.php";
+
+$database = new Database();
+$db = $database->getConn();
+
+$siswa = new Siswa($db);
+$dataSiswa = $siswa->readAll();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Nisn</th>
+            <th>Jurusan</th>
+        </tr>
+            <?php
+                $no = 1;
+                while ($row = $dataSiswa->fetch(PDO::FETCH_ASSOC)) { ?>
+        <tr>
+            <td><?php echo $no++ ?> </td>
+            <td><?php echo $row["nama"] ?> </td>
+            <td><?php echo $row["nisn"] ?> </td>
+            <td><?php echo $row["jurusan"] ?> </td>
+        </tr>
+        <?php } ?>
+    </table>
+</body>
+</html>
